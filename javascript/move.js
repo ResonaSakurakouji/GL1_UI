6e4ed36ac66bcfe45f6c7d887532f524a6842edf
hataruizen <hataruizen@gmail.com>
2024-01-26 15:23:12 +0800

    显示正常了，但是高频点击还是会崩

--- javascript/move.js
+++ javascript/move.js
@@ -27,15 +27,19 @@ let OrderEles = {
             for (let ele_i of OrderEles.eles[i]) {
                 if (ele_i.classList.contains('out_left')) {
                     exed = true;
+                    ele_i.tslXv = vhw_x;
                     ele_i.style.transform = `translateX(${vhw_x}vw)`;
                 } else if (ele_i.classList.contains('out_right')) {
                     exed = true;
+                    ele_i.tslXv = -vhw_x;
                     ele_i.style.transform = `translateX(-${vhw_x}vw)`;
                 } else if (ele_i.classList.contains('out_up')) {
                     exed = true;
+                    ele_i.tslYv = vhw_x;
                     ele_i.style.transform = `translateY(${vhw_x}vh)`;
                 } else if (ele_i.classList.contains('out_down')) {
                     exed = true;
+                    ele_i.tslYv = -vhw_x;
                     ele_i.style.transform = `translateY(-${vhw_x}vh)`;
                 }
             };
@@ -52,15 +56,19 @@ let OrderEles = {
             for (let ele_i of OrderEles.eles[i]) {
                 if (ele_i.classList.contains('out_left')) {
                     exed = true;
+                    ele_i.tslXv = 0;
                     ele_i.style.transform = 'translateX(0vw)';
                 } else if (ele_i.classList.contains('out_right')) {
                     exed = true;
+                    ele_i.tslXv = 0;
                     ele_i.style.transform = 'translateX(0vw)';
                 } else if (ele_i.classList.contains('out_up')) {
                     exed = true;
+                    ele_i.tslYv = 0;
                     ele_i.style.transform = 'translateY(0vh)';
                 } else if (ele_i.classList.contains('out_down')) {
                     exed = true;
+                    ele_i.tslYv = 0;
                     ele_i.style.transform = 'translateY(0vh)';
                 }
             };
@@ -69,7 +77,29 @@ let OrderEles = {
             }
             exed = false;
         };
-    }
+    },
+};
+let TsA2B = {
+    vw2px: function (vwValue) {
+        const vwWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+        const pxValue = (vwValue * vwWidth) / 100;
+        return pxValue;
+    },
+    vh2px: function (vhValue) {
+        const vhHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+        const pxValue = (vhValue * vhHeight) / 100;
+        return pxValue;
+    },
+    px2vw: function (pxValue) {
+        const vwWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+        const vwValue = (pxValue / vwWidth) * 100;
+        return vwValue;
+    },
+    px2vh: function (pxValue) {
+        const vhHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+        const vhValue = (pxValue / vhHeight) * 100;
+        return vhValue;
+    },
 };
 let setOnclick = {
     byEle : function (ele, func) {
@@ -140,13 +170,22 @@ let Move = {
         // if (noClick === true) {return};
         // 获取当前元素的位置
         let rect = this.getBoundingClientRect();
-        let currentX = rect.left + window.scrollX;
-        let currentY = rect.top + window.scrollY;
+        let currentTslXv = 0;
+        let currentTslYv = 0;
+        if (this.tslXv !== undefined) {
+            currentTslXv = TsA2B.vw2px(this.tslXv);
+        };
+        if (this.tslYv !== undefined) {
+            currentTslYv = TsA2B.vh2px(this.tslYv);
+        };
+        // 获取当前元素的 transform 值
+        let currentX = rect.left + window.scrollX - currentTslXv;
+        let currentY = rect.top + window.scrollY - currentTslYv;
         // 计算需要移动的距离
-        let translateX = -currentX;
-        let translateY = -currentY;
+        let translateX = -1* TsA2B.px2vw(currentX);
+        let translateY = -1* TsA2B.px2vh(currentY);
         // 将元素移动到左上角
-        this.style.transform = `translate(${translateX}px, ${translateY}px)`; 
+        this.style.transform = `translate(${translateX}vw, ${translateY}vh)`; 
         let bClickedEleBros = Array.from(this.parentElement.children).filter(child => child !== this);
         OrderEles.init(bClickedEleBros);
     
@@ -155,10 +194,18 @@ let Move = {
     },
     Bk : function() {
         // if (noClick === true) {return};
+        let currentTslXv = 0;
+        let currentTslYv = 0;
+        if (this.tslXv !== undefined) {
+            currentTslXv = this.tslXv;
+        };
+        if (this.tslYv !== undefined) {
+            currentTslYv = this.tslYv;
+        };
+        this.style.transform = `translate(${currentTslXv}vw, ${currentTslYv}vh`; 
         let bClickedEleBros = Array.from(this.parentElement.children).filter(child => child !== this);
         // 所有操作顺序元素归类
         OrderEles.init(bClickedEleBros);
-        this.style.transform = `translate(0, 0)`; 
 
         OrderEles.move_in(100);
         setOnclick.byEle(this, Move.LT);
@@ -174,6 +221,7 @@ let Call = {
 
         OrderEles.move_in(100);
         setOnclick.byEles(mainMenuEles, Move.LT);
+        setOnclick.byEle(this, Move.doNothing);
     },
 };
 window.onload = function() {
