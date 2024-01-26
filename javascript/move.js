842ea4a59c8acdb5696ceb96bf83d5497be24633
hataruizen <hataruizen@gmail.com>
2024-01-26 11:52:13 +0800

    点击空白处可以唤出主菜单了，但是后续动画全是问题

--- javascript/move.js
+++ javascript/move.js
@@ -1,7 +1,7 @@
 var noClick = false;
 var oldClickHandler = [];
 const orderList = ['m1st','m2nd','m3rd','m4th','m5th','m6th','m7th','m8th'];
-let orderEles = {
+let OrderEles = {
     eles : {} ,
     clear : function () {
         for(let i = 0; i < orderList.length; i ++) {
@@ -20,23 +20,23 @@ let orderEles = {
             };
         });
     },
-    move_out : async function(vhw_x) {
+    move_in : async function(vhw_x) {
         noClick = true;
         let exed = false;
         for (let i of orderList) {
-            for (let ele_i of orderEles.eles[i]) {
+            for (let ele_i of OrderEles.eles[i]) {
                 if (ele_i.classList.contains('out_left')) {
                     exed = true;
-                    ele_i.style.transform = `translateX(-${vhw_x}vw)`;
+                    ele_i.style.transform = `translateX(${vhw_x}vw)`;
                 } else if (ele_i.classList.contains('out_right')) {
                     exed = true;
-                    ele_i.style.transform = `translateX(${vhw_x}vw)`;
+                    ele_i.style.transform = `translateX(-${vhw_x}vw)`;
                 } else if (ele_i.classList.contains('out_up')) {
                     exed = true;
-                    ele_i.style.transform = `translateY(-${vhw_x}vh)`;
+                    ele_i.style.transform = `translateY(${vhw_x}vh)`;
                 } else if (ele_i.classList.contains('out_down')) {
                     exed = true;
-                    ele_i.style.transform = `translateY(${vhw_x}vh)`;
+                    ele_i.style.transform = `translateY(-${vhw_x}vh)`;
                 }
             };
             if (exed === true) {
@@ -45,11 +45,11 @@ let orderEles = {
             exed = false;
         };
     },
-    move_in : async function() {
+    move_out : async function() {
         noClick = true;
         let exed = false;
         for (let i of orderList) {
-            for (let ele_i of orderEles.eles[i]) {
+            for (let ele_i of OrderEles.eles[i]) {
                 if (ele_i.classList.contains('out_left')) {
                     exed = true;
                     ele_i.style.transform = 'translateX(0vw)';
@@ -71,74 +71,111 @@ let orderEles = {
         };
     }
 };
+let setOnclick = {
+    byEle : function (ele, func) {
+        if (ele.clickHandlerThis !== undefined) {
+            ele.removeEventListener('click', ele.clickHandlerThis);
+        };
+        let newClickHandler = function() {
+            func.bind(ele)();
+        };
+        ele.addEventListener('click', newClickHandler);
+        ele.clickHandlerThis = newClickHandler;
+    },
+    byEles : function (eles, func) {
+        for (let i = 0; i < eles.length; i++) {
+            if (eles[i].clickHandlerThis !== undefined) {
+                eles[i].removeEventListener('click', eles[i].clickHandlerThis);
+            };
+            let newClickHandler = function() {
+                func.bind(eles[i])();
+            };
+            eles[i].addEventListener('click', newClickHandler);
+            eles[i].clickHandlerThis = newClickHandler;
+        };
+    },
+    byId : function (idName, func) {
+        let ele = document.getElementById(idName);
+        if (ele.clickHandlerThis !== undefined) {
+            ele.removeEventListener('click', ele.clickHandlerThis);
+        };
+        let newClickHandler = function() {
+            func.bind(ele)();
+        };
+        ele.addEventListener('click', newClickHandler);
+        ele.clickHandlerThis = newClickHandler;
+    },
+    byParentId : function (idName, func) {
+        let eles = document.getElementById(idName).children;
+        for (let i = 0; i < eles.length; i++) {
+            if (eles[i].clickHandlerThis !== undefined) {
+                eles[i].removeEventListener('click', eles[i].clickHandlerThis);
+            };
+            let newClickHandler = function() {
+                func.bind(eles[i])();
+            };
+            eles[i].addEventListener('click', newClickHandler);
+            eles[i].clickHandlerThis = newClickHandler;
+        };
+    },
+    byClass : function (className, func) {
+        let eles = document.getElementsByClassName(className);
+        for (let i = 0; i < eles.length; i++) {
+            if (eles[i].clickHandlerThis !== undefined) {
+                eles[i].removeEventListener('click', eles[i].clickHandlerThis);
+            };
+            let newClickHandler = function() {
+                func.bind(eles[i])();
+            };
+            eles[i].addEventListener('click', newClickHandler);
+            eles[i].clickHandlerThis = newClickHandler;
+        };
+    },
+};
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 };
-function move_LT() { 
-    // if (noClick === true) {return};
-    // 获取当前元素的位置
-    let rect = this.getBoundingClientRect();
-    let currentX = rect.left + window.scrollX;
-    let currentY = rect.top + window.scrollY;
-    // 计算需要移动的距离
-    let translateX = -currentX;
-    let translateY = -currentY;
-    // 将元素移动到左上角
-    this.style.transform = `translate(${translateX}px, ${translateY}px)`; 
-    let bClickedEleBros = Array.from(this.parentElement.children).filter(child => child !== this);
-    orderEles.init(bClickedEleBros);
+let Move = {
+    LT : function () { 
+        // if (noClick === true) {return};
+        // 获取当前元素的位置
+        let rect = this.getBoundingClientRect();
+        let currentX = rect.left + window.scrollX;
+        let currentY = rect.top + window.scrollY;
+        // 计算需要移动的距离
+        let translateX = -currentX;
+        let translateY = -currentY;
+        // 将元素移动到左上角
+        this.style.transform = `translate(${translateX}px, ${translateY}px)`; 
+        let bClickedEleBros = Array.from(this.parentElement.children).filter(child => child !== this);
+        OrderEles.init(bClickedEleBros);
+    
+        OrderEles.move_out();
+        setOnclick.byEle(this, Move.Bk);
+    },
+    Bk : function() {
+        // if (noClick === true) {return};
+        let bClickedEleBros = Array.from(this.parentElement.children).filter(child => child !== this);
+        // 所有操作顺序元素归类
+        OrderEles.init(bClickedEleBros);
+        this.style.transform = `translate(0, 0)`; 
 
-    orderEles.move_out(100);
-    setOnclick(this, move_Bk);
+        OrderEles.move_in(100);
+        setOnclick.byEle(this, Move.LT);
+    },
+    doNothing : function() {return;},
 };
-function move_Bk() {
-    // if (noClick === true) {return};
-    let bClickedEleBros = Array.from(this.parentElement.children).filter(child => child !== this);
-    // 所有操作顺序元素归类
-    orderEles.init(bClickedEleBros);
-    this.style.transform = `translate(0, 0)`; 
+let Call = {
+    mainMenu : function() {
+        // if (noClick === true) {return};
+        let mainMenuEles = document.getElementById('mainMenu').children;
+        // 所有操作顺序元素归类
+        OrderEles.init(mainMenuEles);
 
-    orderEles.move_in();
-    setOnclick(this, move_LT);
-};
-function doNothing() {
-    return;
-}
-function setOnclick(ele, func) {
-    if (ele.clickHandlerThis !== undefined) {
-        ele.removeEventListener('click', ele.clickHandlerThis);
-    };
-    let newClickHandler = function() {
-        func.bind(ele)();
-    };
-    ele.addEventListener('click', newClickHandler);
-    ele.clickHandlerThis = newClickHandler;
-};
-function setsOnclick(eles, func) {
-    for (let i = 0; i < eles.length; i++) {
-        if (eles[i].clickHandlerThis !== undefined) {
-            eles[i].removeEventListener('click', eles[i].clickHandlerThis);
-        };
-        let newClickHandler = function() {
-            func.bind(eles[i])();
-        };
-        eles[i].addEventListener('click', newClickHandler);
-        eles[i].clickHandlerThis = newClickHandler;
-    };
-};
-function setSonsOnclick0(idName, func) {
-    let eles = document.getElementById(idName).children;
-    for (let i = 0; i < eles.length; i++) {
-        if (eles[i].clickHandlerThis !== undefined) { // 其实不会执行
-            eles[i].removeEventListener('click', eles[i].clickHandlerThis);
-        };
-        let newClickHandler = function() {
-            func.bind(eles[i])();
-        };
-        eles[i].addEventListener('click', newClickHandler);
-        eles[i].clickHandlerThis = newClickHandler;
-    };
+        OrderEles.move_in(100);
+        setOnclick.byEles(mainMenuEles, Move.LT);
+    },
 };
 window.onload = function() {
-    setSonsOnclick0('mainMenu', move_LT);
+    setOnclick.byId('body',Call.mainMenu);
 };
